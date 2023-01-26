8f6dc1dcab74089aab299a9bddcba54cd908c1ae
Junayed5 <mdjunayed@gmail.com>
2023-01-26 13:03:08 +0600

    half done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Sidebar2 from './component/sidebar2';
 import SideForm from './component/sideForm';
 import Video from './component/Video';
+import Video2 from './component/Video2';
 
 function App() {
   return (
     <div className='grid grid-cols-2 '>
       <Video/>
       <SideForm/>
+      <Video2/>
+      <Sidebar2/>
     </div>
   );
 }
---
+++ src/component/sidebar2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar2 = () => {
+    return (
+        <div>
+            <div className='w-fit mx-auto mt-96'>
+                <button className='h-20 w-72 font-bold text-center text-base text-white rounded-3xl bg-indigo-600 pl-4 px-5 mb-4 '>
+                    Download "Campaign structure Guide"
+                </button><br />
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar2;
